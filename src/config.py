52c54436bdb14cc07955cb0980da2a3b97df25ba
Marlon Ruiz <mjavierrt14@gmail.com>
2023-07-09 15:51:57 -0500

    mensaje variables manuales

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ if (
     or not getenv("MYSQL_DATABASE")
     or not getenv("MYSQL_LOCAL_PORT")
 ):
+    print("Loading environment variables from .env manually...")
     path_to_dotenv = path.join('\\'.join(path.dirname(__file__).split('\\')[:-1]), '.env')
     load_dotenv(dotenv_path=path_to_dotenv)
 
@@ -17,7 +18,6 @@ MYSQL_HOSTNAME = getenv("MYSQL_HOSTNAME")
 MYSQL_PORT = getenv("MYSQL_LOCAL_PORT")
 
 DATABASE_CONNECTION_URI = f"mysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}:{MYSQL_PORT}/{MYSQL_DATABASE}"
-print(DATABASE_CONNECTION_URI)
 
 class Config:
     SECRET_KEY = getenv("SECRET_KEY")
