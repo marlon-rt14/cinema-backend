8fc8ee39a4dc6dcfe3891dd351b47e5aa4593be8
Marlon Ruiz <mjavierrt14@gmail.com>
2023-07-09 12:35:36 -0500

    fixed environments variables

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 from os import getenv
 
-load_dotenv()
-MYSQL_USERNAME = getenv("MYSQL_USERNAME")
+if(not getenv("MYSQL_PASSWORD") or not getenv("MYSQL_DATABASE")or not getenv("MYSQL_PORT")):
+    load_dotenv()
+
+MYSQL_USERNAME = 'root'
 MYSQL_PASSWORD = getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = getenv("MYSQL_DATABASE")
 MYSQL_HOSTNAME = getenv("MYSQL_HOSTNAME")
@@ -16,7 +18,7 @@ class Config():
 class DevelpmentConfig(Config):
     DEBUG = True
     HOST = '0.0.0.0'
-    PORT = 5000
+    PORT = getenv("SERVER_LOCAL_PORT")
     
 config = {
     "development": DevelpmentConfig
